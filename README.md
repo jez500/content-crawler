ba9d1138fb203bc974c2cd5de29ee878a8326c5e
Jeremy <mail@jez500.com>
2019-04-21 16:08:57 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Content Crawler
+
+Crawl a site to get a count of the pages, images, documents and forms it contains. This is ideal if you intend to
+migrate a site and want to get an overview its content.
+
+## Installation
+
+Run `npm install` to include dependencies.
+
+## Starting a crawl
+
+Run `npm run crawl FULL_URL_OF_SITE_TO_CRAWL` replacing `FULL_URL_OF_SITE_TO_CRAWL` with the start url of the site
+you want to crawl. eg `npm run crawl http://example.com/`.
+
+## Viewing crawl results
+
+After a crawl is complete, visit `/public/index.html` to browse the crawl results in a web browser.
+
+## Crawl data
+
+Crawl data is saved as a JSON file in `/public/sites`. This data may be useful for ingesting into other applications 
+or migrations.
+
+## Author
+
+[Jeremy Graham](https://jez.me)
---
+++ index.js
@@ -0,0 +1,194 @@
+const supercrawler = require("supercrawler");
+const extend = require('extend');
+const fs = require('fs-extra');
+const Url = require('url-parse');
+const _ = require('lodash');
+const $ = require('cheerio');
+
+const app = class {
+
+  /**
+   * Class constructor.
+   */
+  constructor(settings = {}) {
+    // Ensure a url to parse
+    const startUrl = process.argv[2];
+    if (!startUrl) {
+      console.error('You must provide a start url as the first argument!');
+      return;
+    }
+
+    // Settings.
+    this.startUrl = new Url(startUrl);
+    this.settings = {
+      domain: this.startUrl.hostname,
+      protocol: this.startUrl.protocol,
+      interval: 1000,
+      saveDir: './public/sites'
+    };
+    this.settings = extend(this.settings, settings);
+
+    // Structure for JSON.
+    this.db = {
+      domain: this.settings.domain,
+      pages: [],
+      images: {},
+      documents: {},
+      forms: {},
+    };
+
+    // Get an instance of the crawler.
+    this.crawler = this.getCrawler();
+
+    // When no more urls to parse.
+    this.crawler.on('urllistcomplete', () => {
+      // Turn assets into arrays.
+      this.db.images = _.keys(this.db.images);
+      this.db.documents = _.keys(this.db.documents);
+      this.db.forms = _.keys(this.db.forms);
+      // Save db to JSON.
+      this.saveDb();
+      this.updateIndex();
+      console.log('Crawl complete!', this.db.pages.length + ' pages', this.db.images.length + ' images',
+        this.db.documents.length + ' documents', this.db.forms.length + ' forms');
+      // Stop crawling.
+      this.crawler.stop();
+    })
+  }
+
+  /**
+   * Return instance of crawler.
+   */
+  getCrawler() {
+    return new supercrawler.Crawler({
+      interval: this.settings.interval,
+      concurrentRequestsLimit: 5,
+      userAgent: "Mozilla/5.0 (compatible; supercrawler/1.0; +https://github.com/brendonboshell/supercrawler)",
+      request: {
+        headers: {
+          'x-custom-header': 'example'
+        }
+      }
+    });
+  }
+
+  /**
+   * Start a new crawl.
+   */
+  startCrawl() {
+    // Restrict to this domain only.
+    this.crawler.addHandler("text", supercrawler.handlers.htmlLinkParser({
+      hostnames: [this.settings.domain]
+    }));
+
+    // Handle links.
+    this.crawler.addHandler("text", (context) => {
+      this.textHander(context);
+    });
+
+    // Handle documents.
+    this.crawler.addHandler("application", (context) => {
+      this.applicationHandler(context);
+    });
+
+    // Start crawl.
+    console.log('Starting crawl from: ' + this.startUrl.href);
+    this.crawler.getUrlList()
+      .insertIfNotExists(new supercrawler.Url(this.startUrl.href))
+      .then(() => {
+        return this.crawler.start();
+      });
+  }
+
+  /**
+   * Handle text mime types (normal html pages).
+   */
+  textHander(context) {
+    // Handler for each page. Add results to db.
+    console.log("Processed", context.url);
+    let res = {
+      title: context.$('title').text(),
+      url: context.url,
+      contentType: context.contentType,
+      size: Math.round((Buffer.byteLength(context.body) / 1024)),
+      forms: this.getForms(context),
+      images: this.getImages(context)
+    };
+    this.db.pages.push(res);
+  }
+
+  /**
+   * Handle application mime types.
+   */
+  applicationHandler(context) {
+    this.db.documents[context.url] = 'document';
+  }
+
+  /**
+   * Get the images found on the page.
+   */
+  getImages(context) {
+    let images = [];
+    context.$('img').each((i ,d) => {
+      let imgUrl = context.$(d).attr('src');
+      this.db.images[ imgUrl ] = 'image';
+      images.push(imgUrl);
+    });
+    return images;
+  }
+
+  /**
+   * Get the forms found on the page.
+   */
+  getForms(context) {
+    let forms = [];
+    context.$('form').each((i ,d) => {
+      let formKey = $(d).html();
+      this.db.forms[ formKey ] = 'form';
+      forms.push($(d).attr('action') + ' - Key: ' + formKey);
+    });
+    return forms;
+  }
+
+  /**
+   * Prepare a dir to save crawled data.
+   */
+  prepareSaveDir() {
+    if (!fs.existsSync(this.settings.saveDir)){
+      fs.mkdirSync(this.settings.saveDir);
+    }
+  }
+
+  /**
+   * Save all the crawled data.
+   */
+  saveDb() {
+    this.prepareSaveDir();
+    let fileName = this.settings.saveDir + '/' + this.settings.domain + '.json';
+    fs.writeJson(fileName, this.db, {spaces: 2})
+      .then(() => { console.log('Database updated'); })
+      .catch(err => { console.log(err); })
+  }
+
+  /**
+   * Updates the index of all crawled sites.
+   */
+  updateIndex() {
+    this.prepareSaveDir();
+    let fileName = this.settings.saveDir + '/index.json';
+    let obj = fs.readJsonSync(fileName, { throws: false });
+    if (!obj) {
+      obj = {};
+    }
+    obj[this.settings.domain] = {title: this.settings.domain, file: this.settings.domain + '.json'};
+    fs.writeJson(fileName, obj, {spaces: 2})
+      .then(() => { console.log('Index updated'); })
+      .catch(err => { console.log(err); })
+  }
+
+};
+
+// Start new crawl.
+let dhcrawl = new app({});
+dhcrawl.startCrawl();
+
